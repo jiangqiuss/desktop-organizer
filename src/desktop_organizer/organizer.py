874409200705f7e91a52d20a07bb80734a5699b4
jiangqiuss <1794473856@qq.com>
2026-07-09 23:19:14 +0800

    Fix categorization

--- src/desktop_organizer/organizer.py
+++ src/desktop_organizer/organizer.py
@@ -28,24 +28,36 @@ SHORTCUT_CATEGORIES = {
         "明日方舟", "鹰角", "启动器", "launcher", "minecraft", "我的世界", "暴雪",
         "blizzard", "origin", "ubisoft", "育碧", "方舟", "终末地", "genshin",
         "valorant", "csgo", "dota", "pubg", "吃鸡", "永劫", "逆水寒", "剑网",
+        "三国杀", "炉石", "守望", "暗黑", "星际", "魔兽", "堡垒之夜", "fortnite",
+        "roblox", "糖豆人", "among us", "apex", "彩虹六号", "刺客信条", "看门狗",
+        "gta", "荒野大镖客", "赛博朋克", "艾尔登法环", "黑神话", "只狼",
+        "植物大战僵尸", "愤怒的小鸟", "开心消消乐", "4399",
     ],
     "社交聊天": [
         "微信", "wechat", "qq", "钉钉", "dingtalk", "飞书", "feishu", "slack",
         "discord", "telegram", "whatsapp", "line", "teams", "会议", "meeting",
-        "classin", "腾讯会议", "zoom", "skype",
+        "classin", "腾讯会议", "zoom", "skype", "企业微信", "陌陌", "探探",
+        "soul", "脉脉", "领英", "linkedin", "twitter", "微博", "weibo",
+        "贴吧", "知乎", "小红书",
     ],
     "影音娱乐": [
         "网易云", "qq音乐", "spotify", "music", "音乐", "bilibili", "b站",
         "哔哩哔哩", "爱奇艺", "优酷", "腾讯视频", "youtube", "抖音", "快手",
         "potplayer", "vlc", "播放器", "player", "录屏", "obs", "直播",
-        "wallpaper", "壁纸",
+        "wallpaper", "壁纸", "酷狗", "酷我", "咪咕", "千千", "foobar",
+        "暴风", "射手", "字幕", "剪映", "pr", "达芬奇", "resolve",
+        "喜马拉雅", "蜻蜓fm", "懒人听书", "猫耳", "荔枝",
     ],
     "工具软件": [
         "todesk", "向日葵", "anydesk", "远程", "remote", "火绒", "杀毒",
         "安全", "antivirus", "bandizip", "7zip", "winrar", "压缩", "解压",
         "图吧", "gpu-z", "cpu-z", "鲁大师", "驱动", "driver", "nvidia",
         "legion", "zone", "工具箱", "toolbox", "snipaste", "截图", "screenshot",
-        "everything", "listary", "搜索", "teracopy", "复制",
+        "everything", "listary", "搜索", "teracopy", "复制", "ccleaner",
+        "清理", "注册表", "分区", "diskgenius", "ghost", "备份", "还原",
+        "ultraiso", "rufus", "ventoy", "pe", "装机", "360", "腾讯电脑管家",
+        "2345", "猎豹", "搜狗输入法", "百度输入法", "讯飞输入法",
+        "wgesture", "quicker", "utools", "powertoys", "seer", "quicklook",
     ],
     "专业软件": [
         "ansys", "autocad", "solidworks", "catia", "ug", "nx", "proe",
@@ -54,20 +66,32 @@ SHORTCUT_CATEGORIES = {
         "嘉立创", "立创", "eda", "altium", "ad", "pads", "cadence",
         "photoshop", "illustrator", "premiere", "after effects", "ae",
         "figma", "sketch", "blender", "maya", "3dmax", "c4d",
+        "revit", "天正", "pkpm", "广联达", "斯维尔", "鲁班",
+        "spss", "stata", "sas", "r语言", "origin", "graphpad",
+        "mathtype", "latex", "overleaf", "wind", "同花顺", "东方财富",
     ],
     "网络工具": [
         "clash", "v2ray", "trojan", "shadowsocks", "ssr", "vpn",
-        "代理", "proxy", "迷雾通", "lantern", "蓝灯",
+        "代理", "proxy", "迷雾通", "lantern", "蓝灯", "加速器",
+        "wireguard", "openvpn", "tailscale", "zerotier",
+        "firefox", "chrome", "edge", "opera", "brave", "vivaldi", "浏览器", "browser",
+        "idm", "aria2", "motrix", "迅雷", "thunder", "qbittorrent", "bt",
     ],
     "办公软件": [
         "wps", "office", "word", "excel", "powerpoint", "outlook",
         "onenote", "notion", "语雀", "飞书文档", "腾讯文档",
         "foxmail", "邮件", "mail", "foxit", "pdf", "福昕",
+        "有道", "翻译", "translate", "百度网盘", "阿里云盘", "onedrive",
+        "dropbox", "坚果云", "天翼", "城通", "蓝奏",
+        "扫描", "ocr", "白描", "汉王",
     ],
     "开发工具": [
-        "visual studio", "vscode", "idea", "pycharm", "webstorm",
+        "visual studio", "vscode", "vs code", "idea", "pycharm", "webstorm",
         "android studio", "xcode", "git", "github", "终端", "terminal",
         "powershell", "cmd", "docker", "postman", "navicat", "dbeaver",
+        "sublime", "notepad++", "vim", "emacs", "cursor", "windsurf",
+        "anaconda", "jupyter", "colab", "redis", "mongo", "mysql",
+        "vmware", "virtualbox", "hyper-v", "wsl",
     ],
 }
 
@@ -77,12 +101,12 @@ JUNK_PATTERNS = ["desktop.ini", "thumbs.db"]
 
 def get_shortcut_category(name: str) -> str:
     """根据快捷方式名字关键词返回细分分类"""
-    name_lower = name.lower()
+    name_lower = name.lower().strip()
     for category, keywords in SHORTCUT_CATEGORIES.items():
         for kw in keywords:
-            if kw.lower() in name_lower:
-                return f"快捷方式-{category}"
-    return "快捷方式-其他"
+            if kw.strip().lower() in name_lower:
+                return category
+    return "其他"
 
 
 def get_file_type(ext: str, name: str = "") -> str:
